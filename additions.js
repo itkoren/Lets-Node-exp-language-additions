c2987f3b9553ce6edd387326cd655f03ccbc95ab
Itai Koren <itkoren@gmail.com>
2014-05-24 14:08:47 +0300

    Initial Commit
    Signed-off-by: Itai Koren <itkoren@gmail.com>

---
+++ additions.js
@@ -0,0 +1,8 @@
+console.log(global);
+course = "Lets Node";
+console.log(global.course);
+console.log(__dirname);
+console.log(__filename);
+console.log(window);
+console.log(document);
+console.log(location);
